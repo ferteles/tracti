3035b0f97ea659153cb3d3bc22c564a5fe9522b7
fernando.teles <fernando.teles@tcu.gov.br>
2026-02-23 09:25:39 -0300

    feat: adiciona componente de rodapé com internacionalização e suas traduções em português.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
   const { t } = useTranslation(); // Use o hook useTranslation
 
   return (
-    <footer className="relative bg-[#500203] text-white py-16 px-4 md:px-8 lg:px-16 min-h-[400px] flex items-center justify-center overflow-hidden">
+    <footer className="relative bg-[#500203] text-white py-16 px-4 md:px-8 lg:px-16 min-h-[688px] flex items-center justify-center overflow-hidden">
       <div
         className="absolute inset-0 bg-cover bg-center opacity-30 z-0"
         style={{ backgroundImage: `url(${bck})` }}
@@ -20,59 +20,81 @@ function Footer() {
         />
       </div>
 
-      <div className=" relative z-10 w-full max-w-screen-xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-16">
-        <div className="flex flex-col justify-center-safe  items-center md:items-start text-center md:text-left">
+      <div className="relative z-10 w-full max-w-screen-xl mx-auto flex flex-col lg:flex-row justify-between items-center gap-12 lg:gap-8 px-4 md:px-0 h-full">
+        <div className="flex flex-col justify-center items-center lg:items-start lg:w-1/3 mt-10 lg:mt-0">
           <img
             src={logo}
             alt="Logo Tracti"
-            className="w-full h-full object-contain max-w-[80%]"
+            className="w-full h-auto object-contain max-w-[250px] md:max-w-[300px] lg:max-w-[350px]"
           />
         </div>
 
-        <div className="flex flex-col items-center md:items-start text-center md:text-left">
-          <h1 className="font-bold text-lg mb-4 uppercase">
-            {t("explore_footer")}
-          </h1>
-          <nav className="flex flex-col space-y-2">
-            <a
-              href="#about"
-              className="text-base hover:underline transition-colors"
-            >
-              {t("quem_somos_nav")}
-            </a>
-            <a
-              href="#service"
-              className="text-base hover:underline transition-colors"
-            >
-              {t("servicos_nav")}
-            </a>
-            <a
-              href="#contacto"
-              className="text-base hover:underline transition-colors"
-            >
-              {t("contacto_nav")}
-            </a>
-            <a
-              href="#Sus"
-              className="text-base hover:underline transition-colors"
-            >
-              {t("sustentabilidade_nav")}
-            </a>
-          </nav>
-          <button className="mt-6 border border-white px-4 py-2 rounded-md text-sm hover:bg-white hover:text-[#500203] transition-all duration-300">
-            {t("entre_em_contacto_btn_nav")}
-          </button>
-        </div>
+        <div className="flex flex-col md:flex-row w-full lg:w-2/3 justify-center lg:justify-end gap-16 md:gap-32">
+          {/* Coluna 1: EXPLORE */}
+          <div className="flex flex-col items-center md:items-start text-center md:text-left">
+            <h1 className="font-normal text-xl md:text-2xl mb-8 uppercase tracking-wider">
+              {t("explore_footer")}
+            </h1>
+            <nav className="flex flex-col space-y-4">
+              <a
+                href="#about"
+                className="text-xs md:text-sm font-light uppercase tracking-wider hover:text-gray-300 transition-colors"
+              >
+                {t("quem_somos_nav")}
+              </a>
+              <a
+                href="#service"
+                className="text-xs md:text-sm font-light uppercase tracking-wider hover:text-gray-300 transition-colors"
+              >
+                {t("servicos_nav")}
+              </a>
+              <a
+                href="#contacto"
+                className="text-xs md:text-sm font-light uppercase tracking-wider hover:text-gray-300 transition-colors"
+              >
+                {t("contacto_nav")}
+              </a>
+              <a
+                href="#Sus"
+                className="text-xs md:text-sm font-light uppercase tracking-wider hover:text-gray-300 transition-colors"
+              >
+                {t("sustentabilidade_nav")}
+              </a>
+              <div className="pt-4">
+                <button className="border border-white px-3 py-1.5 md:px-4 md:py-2 text-xs md:text-sm font-light uppercase tracking-wider hover:bg-white hover:text-[#500203] transition-colors duration-300">
+                  {t("entre_em_contacto_btn_nav")}
+                </button>
+              </div>
+            </nav>
+          </div>
 
-        {/* Coluna 3: CONTACTOS */}
-        <div className="flex flex-col items-center md:items-start text-center md:text-left">
-          <h1 className="font-bold text-lg mb-4 uppercase">
-            {t("contactos_footer")}
-          </h1>
-          <div className="flex flex-col space-y-2 text-base">
-            <h2 className="break-all">{t("footer_email")}</h2>
-            <h2>{t("footer_phone")}</h2>
-            <h2>{t("footer_address")}</h2>
+          {/* Coluna 2: CONTACTOS */}
+          <div className="flex flex-col items-center md:items-start text-center md:text-left max-w-[300px]">
+            <h1 className="font-normal text-xl md:text-2xl mb-8 uppercase tracking-wider">
+              {t("contactos_footer")}
+            </h1>
+            <div className="flex flex-col text-xs md:text-sm font-light uppercase tracking-wider w-full">
+              <a
+                href={`mailto:${t("footer_email")}`}
+                className="break-all mb-4 hover:text-gray-300 transition-colors block"
+              >
+                {t("footer_email")}
+              </a>
+              <a
+                href="tel:+244976334449"
+                className="mb-10 hover:text-gray-300 transition-colors block"
+              >
+                {t("footer_phone")}
+              </a>
+              <a
+                href="https://maps.app.goo.gl/SPVPA5yim1FZSPzR7"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="leading-7 hover:text-gray-300 transition-colors"
+              >
+                {t("footer_address")}
+              </a>
+            </div>
           </div>
         </div>
       </div>
